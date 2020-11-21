db117db989eb3107a432ebe0539cc009823311df
ashen-develops <ashenp.designs@gmail.com>
2020-11-21 12:07:04 -0600

    added tables migrations as well as tests

--- src/app.js
+++ src/app.js
@@ -1,35 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config')
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const {
+    NODE_ENV
+} = require('./config')
+const errorHandler = require('./middleware/error-handler')
+const pancakeRouter = require('./pancake/pancake-router')
 
-const app = express();
+const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = (NODE_ENV === 'production') ?
+    'tiny' :
+    'common';
 
-app.use(morgan(morganOption));
+app.use(morgan(morganOption, {
+    skip: () => NODE_ENV === 'test',
+}))
+app.use(cors())
+app.use(helmet())
 
-app.use(helmet());
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use('/api/pancakes', pancakeRouter)
+app.use(errorHandler)
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
-
-app.use(cors());
-
-module.exports = app;
+module.exports = app
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,13 @@
-const { expect } = require('chai');
-const supertest = require('supertest');
+process.env.TZ = 'UTC'
+process.env.NODE_ENV = 'test'
+require('dotenv').config()
 
-global.expect = expect;
-global.supertest = supertest;
+process.env.TEST_DB_URL = process.env.DB_URL
+
+const {
+    expect
+} = require('chai')
+const supertest = require('supertest')
+
+global.expect = expect
+global.supertest = supertest
